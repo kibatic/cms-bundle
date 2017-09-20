4f2517706ebbfe25ad6e97b27788191c025b5a6e
jcrombez <jeremy.crombez@kibatic.com>
2017-09-20 11:46:13 +0200

    add wysiwyg

--- Form/HtmlBlockType.php
+++ Form/HtmlBlockType.php
@@ -2,8 +2,22 @@
 
 namespace Kibatic\CmsBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+
 class HtmlBlockType extends AbstractBlockType implements BlockTypeInterface
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add('content', TextareaType::class, [
+            'attr' => [
+                'class' => 'wysiwyg'
+            ]
+        ]);
+    }
+
     /**
      * {@inheritdoc}
      */
